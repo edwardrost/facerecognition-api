93b480c6bef7ff074416f12240217181a70a2ac5
edwardrost <edrost.pro@gmail.com>
2019-02-26 12:04:21 +0300

    connect to Heroku Postgres, part2

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-lively-58458',
-    user : 'postgres',
-    password : '020903',
-    database : 'smartbrain'
+    host : process.env.DATABASE_URL,
+    ssl: true,
   }
 });
 
